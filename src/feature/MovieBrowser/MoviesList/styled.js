fddd063d0006f21f2051174d4974bbca86d37df5
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-07 00:13:36 +0200

    Added mediaQuerry

--- src/feature/MovieBrowser/MoviesList/styled.js
+++ src/feature/MovieBrowser/MoviesList/styled.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div` 
     max-width: 1368px;
-    margin: 56px auto 0;
+    margin: 40px auto 0;
+    padding: 16px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin: 24px auto 0;
+        max-width: 320px;
+    }
 `;
 
 export const Header = styled.h1`
     margin-bottom: 24px;
+    max-width: 320px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-bottom: 12px;
+    }
 `;
