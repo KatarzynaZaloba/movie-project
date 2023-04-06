5a77d2c77d952bc2480eb38dcb2c1c2701152778
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-06 16:31:05 +0200

    routes.js added to feature

--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,4 @@ export const toMovie = ({ id } = { id: ":id" }) => `/movies/${id}`;
 
 export const toPeople = () => `/people`;
 export const toPerson = ({ id } = { id: ":id" }) => `/people/${id}`;
+
