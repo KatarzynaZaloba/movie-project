3e6c354659304a4a1a553e0481cee70648a062e8
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-07 10:26:35 +0200

    Implemente RWD to app

--- src/feature/MovieBrowser/MoviesList/styled.js
+++ src/feature/MovieBrowser/MoviesList/styled.js
@@ -7,15 +7,21 @@ export const Wrapper = styled.div`
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         margin: 24px auto 0;
-        max-width: 320px;
+        max-width: 288px;
+        width: 100%;
+        padding: 0;
     }
 `;
 
 export const Header = styled.h1`
     margin-bottom: 24px;
     max-width: 320px;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1.2;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         margin-bottom: 12px;
+        font-size: 18px;
     }
 `;
