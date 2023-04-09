8cad44994fc35ce61fb05de3ff7005043ae54526
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-09 13:11:17 +0200

    Removed unussed styled component

--- src/feature/MovieBrowser/MoviesList/List/index.js
+++ src/feature/MovieBrowser/MoviesList/List/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Details, Genre, Genres, Item, MovieList, Poster, Tile, Title, Wrapper, Year } from './styled'
+import { Details, Genre, Genres, Item, Poster, Tile, Title, Wrapper, Year } from './styled'
 import { data } from "./data"
 import Rating from './Rating'
 
@@ -9,25 +9,24 @@ const List = () => {
 
     return (
         <Wrapper>
-            
-                {movies.map((movie) =>
-                    <Item key={movie}>
-                        <Tile>
-                            <Poster src={movie.poster} alt={movie.title} />
-                            <Details>
-                                <Title>{movie.title}</Title>
-                                <Year>{movie.year}</Year>
-                                <Genres>
-                                    <Genre>{movie.genre}</Genre>
-                                </Genres>
-                                <Rating
-                                    rating={movie.rating}
-                                    votes={movie.votes}
-                                />
-                            </Details>
-                        </Tile>
-                    </Item>
-                )}
+            {movies.map((movie) =>
+                <Item key={movie}>
+                    <Tile>
+                        <Poster src={movie.poster} alt={movie.title} />
+                        <div>
+                            <Title>{movie.title}</Title>
+                            <Year>{movie.year}</Year>
+                            <Genres>
+                                <Genre>{movie.genre}</Genre>
+                            </Genres>
+                            <Rating
+                                rating={movie.rating}
+                                votes={movie.votes}
+                            />
+                        </div>
+                    </Tile>
+                </Item>
+            )}
         </Wrapper>
     )
 }
