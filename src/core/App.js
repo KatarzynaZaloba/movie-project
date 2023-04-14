2a1520bf3c19109574a1af7ccc9fc13738b153d2
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-14 21:14:20 +0200

    duplicate App.js file removed

--- src/core/App.js
+++ src/core/App.js
@@ -1,16 +1,29 @@
-
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Header from "../common/Header";
 import { Pagination } from "../common/Pagination";
-import MovieBrowser from "../feature/MovieBrowser"
+import MovieBrowser from "../feature/MovieBrowser";
+import { MovieDetails } from "../feature/MovieDetails";
+import { toMovies, toMovie } from "../core/routes";
 
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Header />
-      <MovieBrowser />
+      <Switch>
+        <Route path={toMovie()}>
+          <MovieDetails />
+        </Route>
+        <Route path={toMovies()}>
+          <MovieBrowser />
+        </Route>
+
+        <Route>
+          <Redirect to={toMovies()} />
+        </Route>
+      </Switch>
       <Pagination />
-    </>
+    </HashRouter>
   );
 }
 
