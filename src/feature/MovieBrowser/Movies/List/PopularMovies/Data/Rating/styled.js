a49ae45d33626ed050d6f5fcdd060739cb5332e6
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-15 19:11:21 +0200

    Created styled component

---
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/Rating/styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { ReactComponent as star } from "./vector.svg"
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 12px;
+`;
+
+export const Star = styled(star)`
+
+`;
+
+export const Score = styled.p`
+    margin: 0;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+
+    @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px) {
+        font-size: 13px;
+        line-height: 1.3;
+    }
+`;
+
+export const Votes = styled.p`
+    margin: 0;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${({ theme }) => theme.colors.waterloo};
+
+    @media (max-width: ${({theme})=> theme.breakpoints.mobileMax}px) {
+        font-size: 13px;
+        line-height: 1.3;
+    }
+`;
