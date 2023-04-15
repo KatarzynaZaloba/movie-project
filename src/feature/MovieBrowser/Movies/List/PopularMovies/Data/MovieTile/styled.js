00c74062991cc93f547ed5b8d7e086945ceeed1b
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-15 19:10:56 +0200

    Created styled component

---
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/styled.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Tile = styled.div`
+    background-color: ${({ theme }) => theme.colors.white};
+    padding: 16px;
+    box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+    border-radius: 5px;
+
+    @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px) {
+        display: grid;
+        grid-template-columns: auto 1fr; 
+        grid-gap: 16px;
+        width: 100%;
+        height: auto;
+    }
+`;
+
+export const Poster = styled.img`
+    height: 100%;
+    width: 100%;
+    aspect-ratio: 2 / 3;
+    border-radius: 5px;
+    margin-bottom: 16px;
+
+    @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px) {
+        width: 114px;
+        height: auto;
+        margin-bottom: 0;
+    }
+`;
+
+export const Title = styled.h2`
+    line-height: 1.3;
+    font-weight: 500;
+    font-size: 22px;
+    margin: 0;
+
+    @media (max-width: ${({theme})=> theme.breakpoints.mobileMax}px) {
+        font-size: 16px;
+    }
+`;
+
+export const Year = styled.p`
+    font-size: 16px;
+    line-height: 1.5;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.waterloo};
+    margin: 8px 0;
+
+    @media (max-width: ${({theme})=> theme.breakpoints.mobileMax}px) {
+        font-size: 13px;
+    }
+`;
+
