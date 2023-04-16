a95e9a8e6d7e712c8d1d9a7b2d7c43077cb1087f
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-16 23:50:17 +0200

    added genresSaga to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { movieListSaga } from "./feature/MovieBrowser/Movies/List/movieListSaga";
+import { genresSaga } from "./feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/Genres/genresSaga"
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
+        genresSaga(),
     ])
 }
