b08f4eec2c75bbb6a7d508632a6a9706181394ab
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-16 23:52:38 +0200

    Added useEffect to fetchGenres

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/Genres/index.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/Genres/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GenresList, Genre } from './styled'
+import { useDispatch } from 'react-redux'
+import { fetchGenres } from './genresSlice' 
 
-const Genres = () => {
+const Genres = ({ genresId }) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchGenres());
+    }, [dispatch])
 
     return (
         <GenresList>
             <Genre>
+
             </Genre>
+
         </GenresList>
     )
 }
