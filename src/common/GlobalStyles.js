c3a299dc37d515657d6fd22f1fcc069e186fbb8a
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-19 16:32:32 +0200

    padding to body added

--- src/common/GlobalStyles.js
+++ src/common/GlobalStyles.js
@@ -14,6 +14,7 @@ html {
 body {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
+    padding: 16px;
     background-color: ${({ theme }) => theme.colors.whisper};
     color: ${({ theme }) => theme.colors.black}
 }`
