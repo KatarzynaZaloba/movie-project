8481524b81cb03e689603e13f36bbbd704f204b1
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-04-19 17:10:07 +0200

    Update GlobalStyles.js
    removing padding to avoid the space on small screens

--- src/common/GlobalStyles.js
+++ src/common/GlobalStyles.js
@@ -14,7 +14,6 @@ html {
 body {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
-    padding: 16px;
     background-color: ${({ theme }) => theme.colors.whisper};
     color: ${({ theme }) => theme.colors.black}
 }`
