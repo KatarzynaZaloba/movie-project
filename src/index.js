d122fef0ac9c4820536402976d8d25763fbb96de
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-20 12:11:33 +0200

    fixed imports

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ import { theme } from "./common/theme";
 import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { Provider } from "react-redux";
-import store from "./core/store";
 import App from "./core/App/App";
-import { Provider } from "react-redux";
 import store from "./store";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
