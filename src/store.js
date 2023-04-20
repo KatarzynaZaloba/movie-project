54f56a9c6cfa7095d79b8fd65420035a5da20c82
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-20 11:07:38 +0200

    Updated store with genres

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieListReducer from "./feature/MovieBrowser/Movies/List/movieListSlice";
-import genresReducer from "./feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/Genres/genresSlice"
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
 
@@ -9,7 +8,6 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         movieList: movieListReducer,
-        genres: genresReducer,
     },
     middleware: [sagaMiddleware],
 });
