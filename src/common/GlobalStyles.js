fc7f3173155ba262d1a075921879d6adeb9811c2
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-20 14:14:13 +0200

    padding deleted

--- src/common/GlobalStyles.js
+++ src/common/GlobalStyles.js
@@ -14,7 +14,7 @@ html {
 body {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
-    padding: 16px;
+    
 // I will check it how to manage it with media-querries -K.
     background-color: ${({ theme }) => theme.colors.whisper};
     color: ${({ theme }) => theme.colors.black}
