c04c09146b25ac3322479cfe01f203862c5cc68d
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-20 11:07:51 +0200

    Updated rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,8 @@
 import { all } from "redux-saga/effects";
 import { movieListSaga } from "./feature/MovieBrowser/Movies/List/movieListSaga";
-import { genresSaga } from "./feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/Genres/genresSaga"
 
 export default function* rootSaga() {
     yield all([
         movieListSaga(),
-        genresSaga(),
     ])
 }
