0fa18c48c62d72580dc8d4d1ed7df2b477713963
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-21 23:33:02 +0200

    Redesigned styled-component

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/styled.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/styled.js
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.section`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(316px, 1fr));
     gap: 24px;
 `;
 
 export const Item = styled.div`
-    width: 100%;
-    height: 100%;
+    text-decoration: none;
+    margin: 0;
 
     @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px) {
-        max-width: 288px;
+        width: 100%;
     }
 `;
 
