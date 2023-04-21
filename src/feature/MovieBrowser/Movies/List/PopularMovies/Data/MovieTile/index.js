ffbbc2baec6548c479b439ddac3f4297b031b8d9
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-04-21 23:33:51 +0200

    Redesigned index, added if and moved Rating to Details component

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/index.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/MovieTile/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Details, Genre, GenresList, Name, Poster, Tile, Title, Year } from './styled'
+import { Details, Genre, GenresList, Poster, PosterWrapper, Tile, Title, Year } from './styled'
 import Rating from '../Rating'
 
 const MovieTile = ({ movie, genres }) => {
 
     return (
         <Tile>
-            <Poster
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt={movie.title}
-            />
+            <PosterWrapper>
+                {movie.poster_path ? (
+                    <Poster
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                    />
+                ) : (
+                    "123"
+                )}
+            </PosterWrapper>
             <Details>
                 <Title>{movie.title}</Title>
                 <Year>{movie.release_date.slice(0, 4)}</Year>
@@ -17,18 +23,18 @@ const MovieTile = ({ movie, genres }) => {
                     <GenresList>
                         {movie.genre_ids.map((genre_id) => (
                             <Genre key={genre_id}>
-                                <Name>{genres[genre_id]}</Name>
+                                {genres[genre_id]}
                             </Genre>
                         ))}
                     </GenresList>
                 ) : (
                     ""
                 )}
+                <Rating
+                    rating={movie.vote_average}
+                    votes={movie.vote_count}
+                />
             </Details>
-            <Rating 
-            rating={movie.vote_average}
-            votes={movie.vote_count}
-            />
         </Tile>
     )
 }
