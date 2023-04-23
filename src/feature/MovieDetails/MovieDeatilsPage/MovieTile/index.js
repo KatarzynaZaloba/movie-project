e282aa9303dcb3184782d6ee39bd04f0af80f01f
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-23 17:39:56 +0200

    rebuilded the CSS of components for better responsiveness

--- src/feature/MovieDetails/MovieDeatilsPage/MovieTile/index.js
+++ src/feature/MovieDetails/MovieDeatilsPage/MovieTile/index.js
@@ -1,20 +1,21 @@
 import {
-    BlackText,
-    ContentTile,
     ContainerTile,
-    MovieTitle,
-    ImageTitle,
-    Rate,
-    Rating,
-    Year,
-    Description,
-    Tags,
-    Votes,
-    StyledStarIcon,
     WrapperTile,
-    Tag,
-    GreyText,
+    ImageTile,
+    ContentTile,
+    DetailsMovie,
+    MovieTitle,
+    Year,
     Production,
+    GreyText,
+    BlackText,
+    Tags,
+    Tag,
+    Rating,
+    StyledStarIcon,
+    Rate,
+    Votes,
+    Description,
 } from "./styled";
 import poster from "./poster.png"
 
@@ -23,35 +24,37 @@ const MovieTile = () => {
         <>
             <ContainerTile>
                 <WrapperTile>
-                    <ImageTitle src={poster} alt="" />
+                    <ImageTile src={poster} alt="" />
                     <ContentTile>
-                        <MovieTitle>Mulan</MovieTitle>
-                        <Year>2020</Year>
-                        <Production>
-                            <GreyText>Production:</GreyText>
-                            <BlackText>China, USA</BlackText>
-                        </Production >
-                        <Production>
-                            <GreyText>Release date: </GreyText>
-                            <BlackText>24.10.2020</BlackText>
-                        </Production>
-                        <Tags>
-                            <Tag>Action</Tag>
-                            <Tag>Adventure</Tag>
-                            <Tag>Drama</Tag>
-                        </Tags>
-                        <Rating>
-                            <StyledStarIcon /> <Rate>7,8</Rate>
-                            <Votes>/ 10</Votes> <Votes>335 votes</Votes>
-                        </Rating>
-                        <Description>
-                            <div>
-                                A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.
-                            </div>
-                        </Description>
+                        <DetailsMovie>
+                            <MovieTitle>Mulan</MovieTitle>
+                            <Year>2020</Year>
+                            <Production>
+                                <GreyText>Production:</GreyText>
+                                <BlackText>China, USA</BlackText>
+                            </Production >
+                            <Production>
+                                <GreyText>Release date: </GreyText>
+                                <BlackText>24.10.2020</BlackText>
+                            </Production>
+                            <Tags>
+                                <Tag>Action</Tag>
+                                <Tag>Adventure</Tag>
+                                <Tag>Drama</Tag>
+                            </Tags>
+                            <Rating>
+                                <StyledStarIcon /> <Rate>7,8</Rate>
+                                <Votes>/ 10</Votes> <Votes>335 votes</Votes>
+                            </Rating>
+                        </DetailsMovie>
                     </ContentTile>
+                    <Description>
+                        <div>
+                            A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.
+                        </div>
+                    </Description>
                 </WrapperTile>
-            </ContainerTile>
+            </ContainerTile >
         </>
     );
 };
