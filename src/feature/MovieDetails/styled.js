47770c8cf94fde22ead2788ad986456ea2493bfd
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-23 17:41:02 +0200

    rebuilded the CSS of components for better responsiveness

--- src/feature/MovieDetails/styled.js
+++ src/feature/MovieDetails/styled.js
@@ -2,13 +2,11 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div` 
-    max-width: 1400px;
-  padding: 0 16px;
+  max-width: 1368px;
   margin: 0 auto;
 `;
 
 export const DetailsWrapper = styled.div`
-  display: grid;
   margin-top: 64px;
   margin-bottom: 40px;
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
@@ -18,7 +16,7 @@ export const DetailsWrapper = styled.div`
     }
   @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
     margin-top: 16px;
-    margin-bottom: 16px;
+   margin-bottom: 16px;
     grid-gap: 16px;
      }
 `;
