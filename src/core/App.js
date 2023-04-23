6a3d253f2d84371784c6df28075d6a77775bb7b8
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-23 17:43:05 +0200

    App.js added

---
+++ src/core/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
+import MovieDetails from "../feature/MovieDetails";
+import { Pagination } from "../common/Pagination";
+import { Header } from "../common/Header";
+
+
+
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+
+
+            <MovieDetails />
+
+            <Pagination />
+        </BrowserRouter>
+    );
+}
+
+export default App;
