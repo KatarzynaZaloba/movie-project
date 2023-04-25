e40c5c6e4ee6b68e661e14c94550780e8d024a7c
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-25 19:04:21 +0200

    searchSlice and movieDetailsReducer added to store

--- src/store.js
+++ src/store.js
@@ -2,13 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import movieListReducer from "./feature/MovieBrowser/Movies/List/movieListSlice";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
+import searchSlice from './common/Header/Search/searchSlice'
+import movieDetailsReducer from './feature/MovieDetails/movieDeatilsSlice'
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         movieList: movieListReducer,
+        search: searchSlice,
+        movieDetails: movieDetailsReducer,
     },
+
     middleware: [sagaMiddleware],
 });
 
