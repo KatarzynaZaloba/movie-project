1aa5f6a98205e0ce506da350c03cc7760f9df6b8
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-04-25 19:36:49 +0200

    added Pagination component to the Movielist

--- src/feature/MovieBrowser/Movies/index.js
+++ src/feature/MovieBrowser/Movies/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Wrapper } from './styled'
 import List from './List'
+import { Pagination } from '../../../common/Pagination'
 
 const Movies = () => {
     return (
         <Wrapper>
             <List />
+            <Pagination />
         </Wrapper>
     )
 }
