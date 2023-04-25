5ee718a35bbbdb03a21d714817470c38275257b1
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-04-25 19:42:38 +0200

    added logic to the Pagination

--- src/feature/MovieBrowser/Movies/index.js
+++ src/feature/MovieBrowser/Movies/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import { Wrapper } from './styled'
 import List from './List'
 import { Pagination } from '../../../common/Pagination'
 
 const Movies = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+
+    };
     return (
         <Wrapper>
             <List />
-            <Pagination />
+            <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange} />
         </Wrapper>
     )
 }
