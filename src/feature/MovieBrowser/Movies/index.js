6563a8a09e3e62da312497b74b331eb8d2184193
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-04-26 19:08:07 +0200

    connected Pagination with API in MoveList

--- src/feature/MovieBrowser/Movies/index.js
+++ src/feature/MovieBrowser/Movies/index.js
@@ -8,6 +8,22 @@ const Movies = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
 
+    useEffect(() => {
+        const fetchPopularMovies = async () => {
+            try {
+                const response = await fetch(
+                    `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=${currentPage}`
+                );
+                const data = await response.json();
+                setTotalPages(data.total_pages);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchPopularMovies();
+    }, [currentPage]);
+
+
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
 
