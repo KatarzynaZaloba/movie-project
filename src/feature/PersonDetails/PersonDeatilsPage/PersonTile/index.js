d61ad0bce8424df1e983bade602deb08e0c057b0
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-04-27 20:11:21 +0200

    changed MovieTile into PersonTile

--- src/feature/PersonDetails/PersonDeatilsPage/PersonTile/index.js
+++ src/feature/PersonDetails/PersonDeatilsPage/PersonTile/index.js
@@ -5,12 +5,9 @@ import {
     ContentTile,
     DetailsMovie,
     MovieTitle,
-    Year,
     Production,
     GreyText,
     BlackText,
-    Tags,
-    Tag,
     Rating,
     StyledStarIcon,
     Rate,
@@ -27,30 +24,20 @@ const PersonTile = () => {
                     <ImageTile src={poster} alt="" />
                     <ContentTile>
                         <DetailsMovie>
-                            <MovieTitle>Mulan</MovieTitle>
-                            <Year>2020</Year>
+                            <MovieTitle>Liu Yifei</MovieTitle>
                             <Production>
-                                <GreyText>Production:</GreyText>
-                                <BlackText>China, USA</BlackText>
+                                <GreyText>Date of birth:</GreyText>
+                                <BlackText>25.08.1987</BlackText>
                             </Production >
                             <Production>
-                                <GreyText>Release date: </GreyText>
-                                <BlackText>24.10.2020</BlackText>
+                                <GreyText>Place of birth: </GreyText>
+                                <BlackText>Wuhan, Hubei, China</BlackText>
                             </Production>
-                            <Tags>
-                                <Tag>Action</Tag>
-                                <Tag>Adventure</Tag>
-                                <Tag>Drama</Tag>
-                            </Tags>
-                            <Rating>
-                                <StyledStarIcon /> <Rate>7,8</Rate>
-                                <Votes>/ 10</Votes> <Votes>335 votes</Votes>
-                            </Rating>
                         </DetailsMovie>
                     </ContentTile>
                     <Description>
                         <div>
-                            A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father. A young Chinese maiden disguises herself as a male warrior in order to save her father.
+                        Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.
                         </div>
                     </Description>
                 </WrapperTile>
--- src/feature/PersonDetails/PersonDeatilsPage/PersonTile/styled.js
+++ src/feature/PersonDetails/PersonDeatilsPage/PersonTile/styled.js
@@ -3,14 +3,15 @@ import { ReactComponent as StarIcon } from "../Rating/star.svg";
 
 export const ContainerTile = styled.div`
  max-width: 1368px;
- margin: auto;
+ max-height: 100%;
+ margin: 56px auto;
 `;
 
 export const WrapperTile = styled.article`
   margin-top: 64px;
   background: ${({ theme }) => theme.colors.white};
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-  padding: 40px;
+  padding: 40px 40px 40px 40px;
   display: grid;
   justify-items: center;
   grid-column-gap: 36px;
@@ -43,7 +44,7 @@ export const WrapperTile = styled.article`
 `;
 
 export const ImageTile = styled.img`
-margin: 52px;
+margin: 0px;
 width: 100%;
 border-radius: 5px;
 grid-area: poster;
@@ -109,29 +110,9 @@ color: ${({ theme }) => theme.colors.black};
   }
 `;
 
-export const Year = styled.p`
- font-weight: 400;
- font-size: 22px;
- line-height: 1.2;
- display: flex;
- align-items: center;
- margin: 24px 0;
- color: ${({ theme }) => theme.colors.black};
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    font-size: 16px;
-    line-height: 1.3;
-    margin-top: -16px;
-    margin-bottom: 12px;
-  }
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-    font-size: 13px;
-    margin-top: 0;
-    margin-bottom: 8px;
-  }
-`;
 
 export const Production = styled.div`
- margin: 0px 0px 8px 0px;
+ margin: 0px;
  display: flex;
  flex-direction: row;
 `;
@@ -161,51 +142,6 @@ export const BlackText = styled.span`
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
     font-size: 12px;
-  }
-`;
-
-export const Tags = styled.ul`
- list-style: none;
- padding-left: 0px;
- margin: 16px -8px;
- display: flex;
- flex-wrap: wrap;
-   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0;
-    margin: 0;
-  }
-`;
-
-export const Tag = styled.li`
- background: ${({ theme }) => theme.colors.mystic};
- height: 30px;
- padding: 8px 16px;
- font-weight: 400;
- font-size: 14px;
- line-height: 1.4;
- margin: 8px;
- border-radius: 5px;
- border: none;
- display: flex;
- flex-direction: row;
- align-items: center;
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
-    font-size: 10px;
-    line-height: 1.1;
-  }
-`;
-
-export const Rating = styled.div`
- display: flex;
- flex-direction: row;
- align-items: center;
- padding: 0px;
- gap: 12px;
- margin: 0px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-     margin: 0px;
   }
 `;
 
@@ -252,6 +188,8 @@ export const Description = styled.p`
  line-height: 160%;
  margin: 0;
  grid-area: description;
+ word-wrap: wrap;
+ text-align: justify;
  color: ${({ theme }) => theme.colors.black};
   @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     font-size: 18px;
