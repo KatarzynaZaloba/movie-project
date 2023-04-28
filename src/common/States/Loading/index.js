409278e6a1500d10b41afa11033bcf3643dd284d
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-28 19:23:08 +0200

    Loading component created

--- src/common/States/Loading/index.js
+++ src/common/States/Loading/index.js
@@ -1,14 +1,26 @@
+import { useContext } from "react";
+import { SearchResultsContext } from "../../../core/useSearchResults";
+import { useQueryParameter } from "../../../core/QueryBox/useQueryParameter";
+import { searchQueryParamName } from "../../../core/QueryBox/queryParamName";
 import { LoadingContainer, LoadingWrapper, LoadingText, LoadingSpinner } from "./styled";
 
-const Loading = () => (
-    <>
+export const Loading = () => {
+    const query = useQueryParameter(searchQueryParamName);
+    const { isLoading } = useContext(SearchResultsContext);
+
+    if (!isLoading) {
+        return null;
+    }
+
+    return (
         <LoadingContainer>
             <LoadingWrapper>
-                <LoadingText>Search result for "Mulan"</LoadingText>
+                <LoadingText title={!query ? "" : `Search results for "${query}"`} />
                 <LoadingSpinner />
             </LoadingWrapper>
         </LoadingContainer>
-    </>
-);
+    );
+};
 
 export default Loading;
+
