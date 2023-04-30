157f477c9e3e5e75e6726ea9a33233197ecbdc54
Marianna Weychan <mariannaweychan@gmail.com>
2023-04-30 16:18:33 +0200

    added logic for Crew  functionality

--- src/feature/MovieDetails/MovieDeatilsPage/Crew/index.js
+++ src/feature/MovieDetails/MovieDeatilsPage/Crew/index.js
@@ -1,22 +1,34 @@
-import { data } from "../Cast/data";
-import { Wrapper, Poster, Tile, ActorName, List, ActorRole } from "../Cast/styled"
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Wrapper, List, Tile, Poster, ActorName, ActorRole, NoPoster } from "../Cast/styled";
+import { selectCrew } from "../../movieDetailsSlice";
+import { imgBaseUrl } from "../../../../core/APIBox/apiSource";
+import noPosterImg from '../image/noPoster.png';
+//import { toPerson } from "../../../../feature/PersonDetails";
+//jak w Cast :)
 
 const Crew = () => {
-
-    const crew = data.people
+    const crew = useSelector(selectCrew);
 
     return (
         <Wrapper>
-            {crew.map((people) =>
-                <List key={people}>
-                    <Tile>
-                        <Poster src={people.poster} alt={people.name} />
-                        <ActorName>{people.name}</ActorName>
-                        <ActorRole>{people.role}</ActorRole>
-                    </Tile>
+            {crew.map(({ poster_path, name, crew_id, department, id }) => (
+                <List key={crew_id}>
+                    <Link to={toPerson({ personId: id })}>
+                        <Tile>
+                            {poster_path ? (
+                                <Poster src={`${imgBaseUrl}/original/${poster_path}`} alt={name} />
+                            ) : (
+                                <NoPoster src={noPosterImg} alt="No poster available" />
+                            )}
+                            <ActorName>{name}</ActorName>
+                            <ActorRole>{department}</ActorRole>
+                        </Tile>
+                    </Link>
                 </List>
-            )}
+            ))}
         </Wrapper>
     );
-}
+};
+
 export default Crew;
