b59e8f79c86154b1d42035151adcb2e4fa80d94b
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-01 13:21:08 +0200

    updated export Header

--- src/core/App.js
+++ src/core/App.js
@@ -1,5 +1,5 @@
 import { Redirect, Route, Switch } from "react-router-dom";
-import Header from "../common/Header";
+import { Header } from "../common/Header";
 import { toMovie, toMovies, toPeople, toPerson } from "./routes";
 import MoviesList from "../feature/MovieBrowser/Movies";
 import MovieDetails from "../feature/MovieDetails";
