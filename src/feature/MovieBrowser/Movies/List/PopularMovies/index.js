1ade62a280d87c26b8e5ea4e2d67f6b29082abc9
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-01 13:31:44 +0200

    removed Loading double import

--- src/feature/MovieBrowser/Movies/List/PopularMovies/index.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/index.js
@@ -1,7 +1,6 @@
 import Loading from "../../../../../common/States/Loading";
 import Data from "./Data";
 import ErrorBox from "./ErrorBox";
-import Loading from "../../../../../common/States/Loading";
 
 const PopularMovies = ({ status, movies }) => {
     switch (status) {
