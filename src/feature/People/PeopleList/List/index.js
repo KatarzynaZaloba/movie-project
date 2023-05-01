06e77d0e3f496847b5a4d93ba1a7b73507e643eb
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-05-01 22:34:18 +0200

    added Loading component while loading the List

--- src/feature/People/PeopleList/List/index.js
+++ src/feature/People/PeopleList/List/index.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Wrapper, PeopleList, Item, Tile, Poster, Title } from "./styled";
 import { Pagination } from "../../../../common/Pagination";
+import Loading from "../../../../common/States/Loading";
 
 const PopularPeopleList = () => {
   const [people, setPeople] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPopularPeople = async () => {
@@ -14,8 +16,11 @@ const PopularPeopleList = () => {
           `https://api.themoviedb.org/3/person/popular?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&page=${currentPage}`
         );
         const data = await response.json();
-        setPeople(data.results);
-        setTotalPages(data.total_pages);
+        setTimeout(() => {
+          setPeople(data.results);
+          setTotalPages(data.total_pages);
+          setLoading(false);
+        }, 2000);
       } catch (error) {
         console.error(error);
       }
@@ -29,19 +34,23 @@ const PopularPeopleList = () => {
 
   return (
     <Wrapper>
-      <PeopleList>
-        {people.map((person) => (
-          <Item key={person.id}>
-            <Tile>
-              <Poster
-                src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
-                alt={person.name}
-              />
-              <Title>{person.name}</Title>
-            </Tile>
-          </Item>
-        ))}
-      </PeopleList>
+      {loading ? (
+        <Loading />
+      ) : (
+        <PeopleList>
+          {people.map((person) => (
+            <Item key={person.id}>
+              <Tile>
+                <Poster
+                  src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
+                  alt={person.name}
+                />
+                <Title>{person.name}</Title>
+              </Tile>
+            </Item>
+          ))}
+        </PeopleList>
+      )}
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
