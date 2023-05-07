1503d521e23d5b2096253c6f6bb02b0030f351c2
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-07 14:31:54 +0200

    fix code causing component not rendering

--- src/feature/MovieDetails/MovieDeatilsPage/MovieTile/index.js
+++ src/feature/MovieDetails/MovieDeatilsPage/MovieTile/index.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import {
     ContainerTile,
     WrapperTile,
@@ -18,20 +19,14 @@ import {
     Description,
 } from "./styled";
 
-const MovieTile = ({
-    poster_path,
-    original_title,
-    release_date,
-    production_countries,
-    overview,
-    vote_average,
-    vote_count,
-    genres,
-    release_year,
-}) => {
+import { selectMovieDetails } from "../../movieDetailsSlice";
+
+
+const MovieTile = () => {
+    const movieDetails = useSelector(selectMovieDetails);
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        return date.toLocaleDateString("pl-PL", {
+        return date.toLocaleDateString("en-US", {
             year: "numeric",
             month: "long",
             day: "numeric",
@@ -41,38 +36,39 @@ const MovieTile = ({
     return (
         <ContainerTile>
             <WrapperTile>
-                {poster_path && (
-                    <ImageTile src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="Poster" />
+                {movieDetails.poster_path && (
+                    <ImageTile src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+                        alt={movieDetails.title} />
                 )}
                 <ContentTile>
                     <DetailsMovie>
-                        <MovieTitle>{original_title}</MovieTitle>
-                        <Year>{release_year.slice(0, 4)}</Year>
-                        {production_countries.length !== 0 && (
+                        <MovieTitle>{movieDetails.original_title}</MovieTitle>
+                        <Year>{movieDetails.release_date.slice(0, 4)}</Year>
+                        {movieDetails.production_countries.length !== 0 && (
                             <Production>
                                 <GreyText>Production:</GreyText>
-                                <BlackText>{production_countries.map((country) => country.name).join(", ")}</BlackText>
+                                <BlackText>{movieDetails.production_countries.map((country) => country.name).join(", ")}</BlackText>
                             </Production>
                         )}
-                        {release_date && (
+                        {movieDetails.release_date && (
                             <Production>
                                 <GreyText>Release date: </GreyText>
-                                <BlackText>{formatDate(release_date)}</BlackText>
+                                <BlackText>{formatDate(movieDetails.release_date)}</BlackText>
                             </Production>
                         )}
                         <Tags>
-                            {genres.map((genre) => (
+                            {movieDetails.genres.map((genre) => (
                                 <Tag key={genre.id}>{genre.name}</Tag>
                             ))}
                         </Tags>
                         <Rating>
-                            <StyledStarIcon /> <Rate>{vote_average}</Rate>
-                            <Votes>/ 10</Votes> <Votes>{vote_count} votes</Votes>
+                            <StyledStarIcon /> <Rate>{movieDetails.vote_average}</Rate>
+                            <Votes>/ 10</Votes> <Votes>{movieDetails.vote_count} votes</Votes>
                         </Rating>
                     </DetailsMovie>
                 </ContentTile>
                 <Description>
-                    <div>{overview}</div>
+                    <div>{movieDetails.overview}</div>
                 </Description>
             </WrapperTile>
         </ContainerTile>
