b497cd23a6acabcab710c066ab75dee8d2f3c276
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-07 14:31:44 +0200

    fix code causing component not rendering

--- src/feature/MovieDetails/MovieDeatilsPage/Crew/index.js
+++ src/feature/MovieDetails/MovieDeatilsPage/Crew/index.js
@@ -1,28 +1,30 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { Wrapper, List, Tile, Poster, ActorName, ActorRole, NoPoster } from "../Cast/styled";
+import { Wrapper, List, Tile, Poster, ActorName, ActorRole, StyledLink } from "../Cast/styled";
 import { selectCrew } from "../../movieDetailsSlice";
-import posterImg from '../image/poster.svg';
+import noPicture from '../../../../common/Images/noPicture.svg';
 import { toPerson } from "../../../../core/routes";
 
-const Crew = ({ movieId }) => {
-    const crew = useSelector((state) => selectCrew(state, movieId));
+const Crew = () => {
+    const crew = useSelector((state) => selectCrew(state));
 
     return (
         <Wrapper>
             {crew.map(({ profile_path, name, crew_id, department, id }) => (
                 <List key={crew_id}>
-                    <Link to={toPerson({ personId: id })}>
+                    <StyledLink to={toPerson({ personId: id })}>
                         <Tile>
-                            {profile_path ? (
-                                <Poster src={`https://image.tmdb.org/t/p/w185${profile_path}`} alt={name} />
-                            ) : (
-                                <NoPoster src={posterImg} alt="No poster available" />
-                            )}
+                            <Poster
+                                src={`https://image.tmdb.org/t/p/w500/${profile_path}`}
+                                alt={name}
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = noPicture;
+                                }}
+                            />
                             <ActorName>{name}</ActorName>
                             <ActorRole>{department}</ActorRole>
                         </Tile>
-                    </Link>
+                    </StyledLink>
                 </List>
             ))}
         </Wrapper>
