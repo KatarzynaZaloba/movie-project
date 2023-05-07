6bf3b9d4d71117fd67d2d4432bec110697370f24
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-07 14:35:48 +0200

    StyledLink added

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/styled.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.section`
     display: grid;
@@ -10,8 +11,12 @@ export const Item = styled.div`
     text-decoration: none;
     margin: 0;
 
-    @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px) {
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         width: 100%;
     }
 `;
 
+export const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.black};
+`;
