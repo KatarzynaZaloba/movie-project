2313fc79f12a8e18478327027ba11a12d21d10f8
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-07 15:54:23 +0200

    corrected App.js

--- src/core/App.js
+++ src/core/App.js
@@ -5,24 +5,20 @@ import MoviesList from "../feature/MovieBrowser/Movies";
 import PeopleList from "../feature/People";
 import PersonDetails from "../feature/PersonDetails"
 import MovieDetails from "../feature/MovieDetails";
-import { Pagination } from "../common/Pagination";
 
 
 function App() {
   return (
-    <>
-      <HashRouter>
-        <Header />
-        <Switch>
-          <Route path={toMovie()} name="movie" component={MovieDetails} />
-          <Route exact path={toMovies()} name="movies" component={MoviesList} />
-          <Route path={toPerson()} name="person" component={PersonDetails} />
-          <Route path={toPeople()} name="people" component={PeopleList} />
-          <Route path="/" component={MoviesList} />
-        </Switch>
-        <Pagination />
-      </HashRouter>
-    </>
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route path={toMovie()} name="movie" component={MovieDetails} />
+        <Route exact path={toMovies()} name="movies" component={MoviesList} />
+        <Route path={toPerson()} name="person" component={PersonDetails} />
+        <Route path={toPeople()} name="people" component={PeopleList} />
+        <Route path="/" component={MoviesList} />
+      </Switch>
+    </HashRouter>
   );
 }
 
