c4227c4117592abc0e13bea90e25c7c3b4f84530
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-07 14:22:13 +0200

    code for displaying search results has been rebuilt

--- src/feature/MovieBrowser/Movies/List/PopularMovies/Data/index.js
+++ src/feature/MovieBrowser/Movies/List/PopularMovies/Data/index.js
@@ -1,28 +1,64 @@
-import { useSelector } from "react-redux"
-import MovieTile from "./MovieTile"
-import { Item, Wrapper } from "./styled"
-import { selectGenres } from "../../movieListSlice"
-import { Header } from "../../../styled"
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import MovieTile from "./MovieTile";
+import { Item, Wrapper, StyledLink } from "./styled";
+import { selectGenres, selectMovies } from "../../movieListSlice";
+import { Header } from "../../../styled";
+import NoResults from "../../../../../../common/NoResults";
+import { useQueryParameter } from "../../../../../../core/QueryBox/useQueryParameter";
+import { searchQueryParamName } from "../../../../../../core/QueryBox/queryParamName";
+import { toMovie } from "../../../../../../core/routes";
 
-const Data = ({ movies }) => {
-    
-    const genres = useSelector(selectGenres)
-    
+const Data = () => {
+    const history = useHistory();
+    const query = useQueryParameter(searchQueryParamName);
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const movies = useSelector(selectMovies);
+    const genres = useSelector(selectGenres);
+
+    useEffect(() => {
+        setFilteredMovies(
+            query
+                ? movies.filter((movie) =>
+                    movie.title.toLowerCase().includes(query.toLowerCase())
+                )
+                : movies
+        );
+    }, [query, movies]);
+
+    const handleClick = (movieId) => {
+        history.push(toMovie({ movieId: movieId }));
+    };
+
     return (
         <>
-        <Header>Popular Movies</Header>
-        <Wrapper>
-            {movies.map((movie) =>
-                <Item key={movie.id}>
-                    <MovieTile
-                        movie={movie}
-                        genres={genres}
-                    />
-                </Item>
+            {!movies.length ? (
+                <NoResults />
+            ) : (
+                <>
+                    <Header>
+                        {!query
+                            ? "Popular Movies"
+                            : `Search results for "${query}" (${filteredMovies.length})`}
+                    </Header>
+                    <Wrapper>
+                        {filteredMovies.map((movie) => (
+                            <Item key={movie.id}>
+                                <StyledLink to={toMovie({ movieId: movie.id })}>
+                                    <MovieTile
+                                        movie={movie}
+                                        genres={genres}
+                                        onClick={() => handleClick(movie.id)}
+                                    />
+                                </StyledLink>
+                            </Item>
+                        ))}
+                    </Wrapper>
+                </>
             )}
-        </Wrapper >
         </>
-    )
-}
+    );
+};
 
-export default Data
+export default Data;
