1deef859c3ac4c63cf45a92af709cf506b0290ac
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-08 10:46:23 +0200

    Changed look of component

--- src/feature/MovieBrowser/index.js
+++ src/feature/MovieBrowser/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import MoviesList from './MoviesList'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchGenres, fetchMovie, selectMovies, selectMovieStatus } from '../../movieBrowserSlice'
 
+
+    const dispatch = useDispatch();
+
+    // const movies = useSelector(selectMovies);
+    const movieStatus = useSelector(selectMovieStatus);
+
+    useEffect(() => {
+        dispatch(fetchMovie());
+        dispatch(fetchGenres());
+    }, [dispatch]);
+    
 const MovieBrowser = () => {
-    return (
-        <MoviesList />
-    )
+    switch (movieStatus) {
+        case "initial":
+            return null;
+
+        case "loading":
+            return <Loading />;
+
+        case "error":
+            return <ErrorBox />;
+
+        case "success":
+            return <Data movies={movies} />;
+
+        default:
+            throw new Error(`incorrect status: ${movieStatus}`);
+    }
 }
 
 export default MovieBrowser
