b3f46bf92f4c84f1be6ebb7df57fa8074471dc36
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-10 09:16:00 +0200

    Removed Padding

--- src/feature/MovieBrowser/Movies/styled.js
+++ src/feature/MovieBrowser/Movies/styled.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export const Wrapper = styled.div` 
     max-width: 1368px;
     margin: 52px auto 40px;
-    padding: 0 16px;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
         padding: 0 12px;
