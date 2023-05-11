f980bf2d36bc112ba9666414278d1e9a6c3438b9
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-11 08:42:16 +0200

    added comment for path toPerson

--- src/core/routes.js
+++ src/core/routes.js
@@ -2,3 +2,5 @@ export const toMovies = () => '/movies';
 export const toMovie = ({ movieId }) => `/movies/${movieId}`;
 export const toPeople = () => '/people';
 export const toPerson = ({ personId } = { personId: ':personId' }) => `/person/${personId}`;
+
+//export const toPerson = () => '/person/:personId';
