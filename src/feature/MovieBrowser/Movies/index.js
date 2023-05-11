f963924cf626f5d07ad5c5d9fa2b89f1793d088e
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-12 00:03:24 +0200

    Added copy/paste exact page from url to app.

--- src/feature/MovieBrowser/Movies/index.js
+++ src/feature/MovieBrowser/Movies/index.js
@@ -6,24 +6,28 @@ import MovieTile from '../../../common/MovieTile';
 import NoResults from '../../../common/NoResults';
 import { Wrapper, Item, TileWrapper, StyledLink, Header } from './styled';
 import { selectGenres } from '../movieBrowserSlice';
-import { searchQueryParamName } from '../../../core/QueryBox/queryParamName';
+import { searchQueryParamName, pageQueryParamName } from '../../../core/QueryBox/queryParamName';
 import { toMovie } from '../../../core/routes';
 import LoadingSpinnerOnly from '../../../common/States/Loading/LoadingSpinnerOnly';
 import LoadingSearchResults from '../../../common/States/Loading/LoadingSearchResult';
 
+
 const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
+    const [totalPages, setTotalPages] = useState();
     const [searchResults, setSearchResults] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const genres = useSelector(selectGenres);
     const location = useLocation();
     const history = useHistory();
 
-
     let searchQuery = new URLSearchParams(location.search).get(searchQueryParamName);
 
+    const pageQuery = new URLSearchParams(location.search).get(
+        pageQueryParamName
+    );
+        
     useEffect(() => {
         const fetchMovies = async () => {
             setIsLoading(true);
@@ -31,12 +35,19 @@ const Movies = () => {
                 const searchQuery = new URLSearchParams(location.search).get(
                     searchQueryParamName
                 );
+
+                const pageQuery = new URLSearchParams(location.search).get(
+                    pageQueryParamName
+                );
+
                 let endpoint;
                 if (searchQuery) {
-                    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&query=${searchQuery}&page=${currentPage}`;
+                    endpoint = `https://api.themoviedb.org/3/search/movie?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&query=${searchQuery}&page=${pageQuery? pageQuery : currentPage}`;
                 } else {
-                    endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&page=${currentPage}`;
-                }
+                    endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&page=${pageQuery}`;}
+                // } else {
+                //     endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&page=${currentPage}`;
+                // }
                 const response = await fetch(endpoint);
                 const data = await response.json();
                 if (searchQuery) {
@@ -54,12 +65,13 @@ const Movies = () => {
             } catch (error) {
                 console.error(error);
             }
+            (pageQuery) ? setCurrentPage(pageQuery) : setCurrentPage(1);
             setTimeout(() => {
                 setIsLoading(false);
             }, 1000);
         };
         fetchMovies();
-    }, [currentPage, location.search, searchQuery]);
+    }, [currentPage, location.search, searchQuery, pageQuery]);
 
 
     const renderHeader = () => {
@@ -89,7 +101,7 @@ const Movies = () => {
             {isLoading && !searchQuery && <LoadingSpinnerOnly />}
             {isLoading && searchQuery && <LoadingSearchResults />}
             {!isLoading && (
-                <TileWrapper>
+                <TileWrapper> 
                     {movies.length > 0 ? (
                         movies.map((movie) => (
                             <Item key={movie.id}>
