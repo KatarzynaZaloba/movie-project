ce28fe2d4d3c08323e6318ebd1194c794e500230
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-14 11:09:11 +0200

    fixed height of loader

--- src/feature/People/PeopleList/List/styled.js
+++ src/feature/People/PeopleList/List/styled.js
@@ -4,8 +4,6 @@ import { NavLink } from 'react-router-dom';
 export const Wrapper = styled.div`
     max-width: 1368px;
     margin: 56px auto 0;
-    margin: 0 auto;
-    padding: 0 16px;
     margin-bottom: 100px;
 `;
 
