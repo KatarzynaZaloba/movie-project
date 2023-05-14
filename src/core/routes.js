76c1473e2f9f80c5af56170e2fbe2577dc0cee91
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-14 11:35:23 +0200

    deleted comment

--- src/core/routes.js
+++ src/core/routes.js
@@ -2,5 +2,3 @@ export const toMovies = () => '/movies';
 export const toMovie = ({ movieId }) => `/movies/${movieId}`;
 export const toPeople = () => '/people';
 export const toPerson = ({ personId } = { personId: ':personId' }) => `/people/${personId}`;
-
-//export const toPerson = () => '/person/:personId';
