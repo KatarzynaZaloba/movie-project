6a562718733e6d9dc40de28c98b82d587cee93f8
Marianna Weychan <mariannaweychan@gmail.com>
2023-05-15 19:51:09 +0200

    setTimeout added to useEffect

--- src/feature/PersonDetails/index.js
+++ src/feature/PersonDetails/index.js
@@ -12,22 +12,25 @@ const PersonDetails = () => {
 
   const { personId } = useParams();
   const [personDetails, setPersonDetails] = useState(null);
-  const [error, setError] = useState(null); // new state
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`https://api.themoviedb.org/3/person/${personId}?api_key=d3f19b5007aaab7cb579f83b9a664dec&language=en-US&append_to_response=movie_credits`);
         const result = await response.json();
-        setPersonDetails(result);
+        setTimeout(() => {
+          setPersonDetails(result);
+        }, 2000);
       } catch (error) {
         console.error(error);
-        setError(error); // error setting
+        setError(error);
       }
     };
 
     fetchData();
   }, [personId]);
+
 
   if (!personDetails) {
     return (
@@ -39,7 +42,7 @@ const PersonDetails = () => {
   if (error || !personDetails || !personDetails.movie_credits) {
     return <ErrorBox error={error} />;
   }
-  
+
   return (
     <Wrapper>
       <DetailsWrapper>
@@ -54,7 +57,7 @@ const PersonDetails = () => {
             />
           </>
         )}
-  
+
         {personDetails.movie_credits.crew.length > 0 && (
           <>
             <SectionTitle>
