20bf2f16f1dd44228547ee8dca5313c12ef9a115
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2023-05-16 11:12:02 +0200

    Added responsivness and imported picture as reactComponent into styledComponent and

--- src/common/States/NoResults/styled.js
+++ src/common/States/NoResults/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { ReactComponent as picture } from "../NoResults/noResultsIcon.svg";
 
 export const Wrapper = styled.div`
   max-width: 1368px;
@@ -6,21 +7,21 @@ export const Wrapper = styled.div`
 `;
 
 export const Disclaimer = styled.div`
-  max-height: 43px;
   font-style: normal;
   font-weight: 600;
   font-size: 36px;
   line-height: 120%;
   margin-left: 20px;
   color: ${({ theme }) => theme.colors.woodsmoke};
-  @media (max-width: ${({ theme }) => theme.breakpoints.desktopHorizontalMax}px) {
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
     font-size: 30px;
   }
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileM}px) {
-    font-size: 15px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 20px;
   }
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileS}px) {
-    font-size: 10px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+    font-size: 16px;
   }
 `;
 
@@ -30,5 +31,30 @@ export const PictureWrapper = styled.div`
   justify-content: center;
   align-items: center;
   max-width: 1920px;
-  margin-top: 188px;
+  margin-top: 120px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    margin-top: 100px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    margin-top: 70px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+    margin-top: 40px;
+  }
 `;
+
+export const Picture = styled(picture)`
+  width: 400px;
+  height: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    width: 300px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    width: 250px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+    width: 150px;
+  }
+`
